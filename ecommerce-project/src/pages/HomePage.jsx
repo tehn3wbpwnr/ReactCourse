ff9377cf580bf9bb528da606788130e83719e2d7
Joshua Moore <Jmoore0177@conestogac.on.ca>
2026-01-14 16:58:49 -0500

    added back end from starting code and fetched data in the homepage

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+                console.log(data)
+            })
+
     return (
         <>
             <title>Ecommerce Project</title>
