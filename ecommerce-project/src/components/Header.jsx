ad01f8245624ae3766f69232aaab57c631bc7492
Joshua Moore <Jmoore0177@conestogac.on.ca>
2026-01-14 17:39:34 -0500

    get cart from backend and display qunatity

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -5,7 +5,12 @@ import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 
-export function Header() {
+export function Header({ cart }) {
+    let totalQuantity = 0;
+
+    cart.forEach(cartItem => {
+        totalQuantity += cartItem.quantity;
+    });
     return (
         <div className="header">
             <div className="left-section">
@@ -33,7 +38,7 @@ export function Header() {
 
                 <NavLink className="cart-link header-link" to="/checkout">
                     <img className="cart-icon" src={CartIcon} />
-                    <div className="cart-quantity">3</div>
+                    <div className="cart-quantity">{totalQuantity}</div>
                     <div className="cart-text">Cart</div>
                 </NavLink>
             </div>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -7,12 +7,19 @@ import './HomePage.css';
 
 export function HomePage() {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then((response) => {
                 setProducts(response.data)
             });
+
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                console.log('cart response:', response.data);
+                setCart(response.data)
+            })
     }, []); //[] dependency array to tell useEffect to run once
 
 
@@ -20,7 +27,7 @@ export function HomePage() {
         <>
             <title>Ecommerce Project</title>
             <link rel="icon" type="image/svg+xml" href="/images/icons/home-favicon.png" />
-            <Header />
+            <Header cart={cart} />
             <div className="home-page">
                 <div className="products-grid">
                     {products.map((products) => {
