0e67f30de16cfd0af2a43b9df391be1b227e52fa
Joshua Moore <Jmoore0177@conestogac.on.ca>
2026-01-14 17:44:53 -0500

    add server proxy config for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -10,12 +10,12 @@ export function HomePage() {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
             .then((response) => {
                 setProducts(response.data)
             });
 
-        axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
             .then((response) => {
                 console.log('cart response:', response.data);
                 setCart(response.data)
