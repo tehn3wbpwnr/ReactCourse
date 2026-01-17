2e0159c53b90216c81402ed94193a5db3f102d49
Joshua Moore <Jmoore0177@conestogac.on.ca>
2026-01-16 23:37:49 -0500

    working on exercises, continuing to break html down into smaller components

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -10,10 +10,12 @@ export function OrdersPage({ cart }) {
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
-        axios.get('/api/orders?expand=products')
-            .then((response) => {
-                setOrders(response.data)
-            })
+        const fetchOrdersData = async () => {
+            let response = await axios.get('/api/orders?expand=products')
+            setOrders(response.data)
+        }
+
+        fetchOrdersData();
     }, []);
 
     return (
