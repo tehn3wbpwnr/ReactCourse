f24174262ddced7baaa746c96702f4ee51803879
Joshua Moore <Jmoore0177@conestogac.on.ca>
2026-04-13 15:05:00 -0400

    delete cart item feature added

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -12,7 +12,7 @@ export function OrderSummary({ deliveryOptions, cart, loadCart }) {
                         <DeliveryDate deliveryOptions={deliveryOptions} cartItem={cartItem} />
 
                         <div className="cart-item-details-grid">
-                            <CartItemDetails cartItem={cartItem} />
+                            <CartItemDetails cartItem={cartItem} loadCart={loadCart}/>
                             <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart={loadCart} />
                         </div>
                     </div>
