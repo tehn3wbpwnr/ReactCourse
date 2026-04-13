32cb55d8c3a638e04b56367577f8b7b0cd5cd04a
Joshua Moore <Jmoore0177@conestogac.on.ca>
2026-04-13 14:43:58 -0400

    create add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -12,19 +12,19 @@ import { useState } from 'react';
 function App() {
   const [cart, setCart] = useState([]);
 
-  useEffect(() => {
-    const fetchAppData = async () => {
+  const loadCart = async () => {
       const response = await axios.get('/api/cart-items?expand=product')
       setCart(response.data)
     }
 
-    fetchAppData();
+  useEffect(() => {
+    loadCart();
   }, [])//[] dependency array to tell useEffect to run once
 
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart = {loadCart}/>} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart}/>} />
